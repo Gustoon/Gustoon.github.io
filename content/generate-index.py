e05251a6ad3642be2f2a5e0338ce4d9cf4370048
Gustoon <135733758+Gustoon@users.noreply.github.com>
2025-05-14 14:47:55 +0200

    ?

--- content/generate-index.py
+++ content/generate-index.py
@@ -7,37 +7,38 @@ indexTextStart = """<!DOCTYPE html>
     <h2>Index of {folderPath}</h2>
     <hr>
     <ul>
-		<li>
-			<a href='../'>../</a>
-		</li>
+        <li>
+            <a href='../'>../</a>
+        </li>
 """
 indexTextEnd = """
-	</ul>
+    </ul>
 </body>
 </html>
 """
 
 def index_folder(folderPath):
-	print("Indexing: " + folderPath +'/')
-	#Getting the content of the folder
-	files = os.listdir(folderPath)
-	#If Root folder, correcting folder name
-	root = folderPath
-	if folderPath == '.':
-		root = 'Root'
-	indexText = indexTextStart.format(folderPath=root)
-	for file in files:
-		#Avoiding index.html files
-		if file != 'index.html':
-			indexText += "\t\t<li>\n\t\t\t<a href='" + file + "'>" + file + "</a>\n\t\t</li>\n"
-		#Recursive call to continue indexing
-		if os.path.isdir(folderPath+'/'+file):
-			index_folder(folderPath + '/' + file)
-	indexText += indexTextEnd
-	#Create or override previous index.html
-	index = open(folderPath+'/index.html', "w")
-	#Save indexed content to file
-	index.write(indexText)
+    print("Indexing: " + folderPath + '/')
+    # Getting the content of the folder
+    files = os.listdir(folderPath)
+    # If Root folder, correcting folder name
+    root = folderPath
+    if folderPath == '.':
+        root = 'Root'
+    indexText = indexTextStart.format(folderPath=root)
+    for file in files:
+        full_path = os.path.join(folderPath, file)
+        # Skip index.html files and "Survie21" directory
+        if file == 'index.html' or file == 'Survie21':
+            continue
+        indexText += f"\t\t<li>\n\t\t\t<a href='{file}'>{file}</a>\n\t\t</li>\n"
+        # Recursive call to continue indexing
+        if os.path.isdir(full_path):
+            index_folder(full_path)
+    indexText += indexTextEnd
+    # Create or override previous index.html
+    with open(os.path.join(folderPath, 'index.html'), "w") as index:
+        index.write(indexText)
 
-#Indexing root directory (Script position)
+# Indexing root directory (script location)
 index_folder('.')
